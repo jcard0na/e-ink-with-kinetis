f82f5d27ea5045acebe5213ca4378b998098c0bb
Javier Cardona <javier.cardona@gmail.com>
2016-07-18 13:55:46 -0700

    Okio smile

--- source/display.c
+++ source/display.c
@@ -9,6 +9,7 @@
 #include "string.h"
 #include "stdio.h"
 #include "display.h"
+#include "okio_bubble.h"
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 #define HEIGHT (200)
@@ -115,12 +116,14 @@ void display_watch(unsigned char hour, unsigned char minute, unsigned char secon
         u8g_DrawDisc(&u8g, WIDTH/2, HEIGHT/2, HEIGHT/2 - 40, U8G_DRAW_ALL);
         u8g_SetColorIndex(&u8g, 0);
         u8g_DrawDisc(&u8g, WIDTH/2, HEIGHT/2, HEIGHT/2 - 45, U8G_DRAW_ALL);
+        draw_watch(NULL);
         u8g_SetColorIndex(&u8g, 1);
 
-        u8g_SetFont(&u8g, u8g_font_profont22r);
-        u8g_SetFontPosCenter(&u8g);
-        w = u8g_GetStrWidth(&u8g, "OKIO");
-        u8g_DrawStr(&u8g, WIDTH/2 - w/2, HEIGHT/2, "OKIO");
+        u8g_DrawBitmap(&u8g, WIDTH/2 - 64/2, HEIGHT/2 - 64/2, 64/8, 64, okio_bubble_64);
+        /*u8g_SetFont(&u8g, u8g_font_profont22r);*/
+        /*u8g_SetFontPosCenter(&u8g);*/
+        /*w = u8g_GetStrWidth(&u8g, "OKIO");*/
+        /*u8g_DrawStr(&u8g, WIDTH/2 - w/2, HEIGHT/2, "OKIO");*/
 
         /*draw_hands(hour % 12, minute, second);*/
     } while (u8g_NextPage(&u8g));
--- source/u8glib/dev/u8g_dev_gdep015oc1.c
+++ source/u8glib/dev/u8g_dev_gdep015oc1.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include "Display_EPD_W21.h"
 #include "Display_EPD_W21_spi.h"
-#include "photo.h"
 #include "../u8g.h"
 
 
