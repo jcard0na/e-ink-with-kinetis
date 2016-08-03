8e543e67c16c53a17a7a559f8e01f515b517d108
Javier Cardona <javier.cardona@gmail.com>
2016-08-02 19:45:51 -0700

    WIP: creating new device for color part

--- source/u8glib/dev/u8g_dev_gdep015oc1.c
+++ source/u8glib/dev/u8g_dev_gdep015oc1.c
@@ -9,10 +9,9 @@
 #include "../u8g.h"
 #include "okio_logo_no_mouth.h"
 #include "okio_logo_only_mouth.h"
-#include "qrcode.h"
 
 /*
- * The GDEP015OC1 accepts 1-byte worth of pixels at a time, corresponding to 8
+ * The GDEW0154T1 accepts 1-byte worth of pixels at a time, corresponding to 8
  * sequential pixels along the X (horizontal) axis.
  *
  * We will use a single page for the entire display.
@@ -22,14 +21,11 @@
 #include <stddef.h>
 
 
-/* Supported options are 8, 32 or 200.  Larger sizes will require more memory
- * but update faster the display.
- */
 #define ROWS_PER_PAGE 200
 #define ROWS_PER_PAGE_IN_BYTES ((ROWS_PER_PAGE + 7) / 8)                    /* i.e. 25 */
-#define GDEP015OC1_ROWS 200
-#define GDEP015OC1_COLUMNS 200
-#define GDEP015OC1_BYTES_PER_LINE ((GDEP015OC1_COLUMNS + 7) / 8)    /* i.e. 25 */
+#define GDEW0154T1_ROWS 200
+#define GDEW0154T1_COLUMNS 200
+#define GDEW0154T1_BYTES_PER_LINE ((GDEW0154T1_COLUMNS + 7) / 8)    /* i.e. 25 */
 
 // this is undocumented voodoo from the vendor for full screen update mode
 const unsigned char lut_vcom0[] ={	0x0E	,0x14	,0x01	,0x0A	,0x06	,0x04	,0x0A	,0x0A	,0x0F	,0x03	,0x03	,0x0C	,0x06	,0x0A	,0x00	};
@@ -44,15 +40,15 @@ const unsigned char lut_red1[] ={	0x03	,0x1D	,0x01	,0x01	,0x08	,0x23	,0x37	,0x37
 
 
 
-static uint8_t cgram_[ROWS_PER_PAGE * GDEP015OC1_BYTES_PER_LINE];
+static uint8_t cgram_[ROWS_PER_PAGE * GDEW0154T1_BYTES_PER_LINE];
 
 static u8g_pb_t pb = {
     { /* u8g_page_t */
         ROWS_PER_PAGE,
-        GDEP015OC1_ROWS,
+        GDEW0154T1_ROWS,
         0, 0, 0
     },
-    GDEP015OC1_COLUMNS,
+    GDEW0154T1_COLUMNS,
     cgram_,
 };
 
@@ -87,23 +83,24 @@ static uint8_t expand_nibble(uint8_t byte, bool upper_nibble)
     return extended;
 }
 
-static void Ultrachip(void)
+static void draw_pic_data_bw(const uint8_t *data)
 {
 	unsigned int i;
 	for(i=0;i<5000;i++)
 	{
-		EPD_W21_WriteDATA(expand_nibble(qrcode[i], true));
-		EPD_W21_WriteDATA(expand_nibble(qrcode[i], false));
+        // bw uses to bits per pixel, 11 is black, 00 is white
+		EPD_W21_WriteDATA(expand_nibble(data[i], true));
+		EPD_W21_WriteDATA(expand_nibble(data[i], false));
 	}
 	driver_delay_xms(2);
 }
 
-static void Ultrachip_red(void)
+static void draw_pic_data_red(const uint8_t *data)
 {
     unsigned int i;
     for(i=0;i<5000;i++)
 	{
-		EPD_W21_WriteDATA(0x00);
+		EPD_W21_WriteDATA(data[i]);
 	}
 	driver_delay_xms(2);
 }
@@ -111,9 +108,9 @@ static void Ultrachip_red(void)
 static void pic_display(void)
 {
 	EPD_W21_WriteCMD(0x10);			//¿ªÊ¼´«ÊäºÚ°×Í¼Ïñ
-	Ultrachip();
+	draw_pic_data_bw(okio_logo_no_mouth);
 	EPD_W21_WriteCMD(0x13);			//¿ªÊ¼´«ÊäºìÍ¼Ïñ
-	Ultrachip_red();
+	draw_pic_data_red(okio_logo_only_mouth);
 }
 
 
@@ -157,32 +154,67 @@ static void lut_red(void)
 		{EPD_W21_WriteDATA(lut_red1[count]); }
 }
 
+static void lcd_chkstatus(void)
+{
+    while(1)
+    {
+        if(isEPD_W21_BUSY==0) break;
+    }
+    driver_delay_xms(2000);
+}
+
 
 static int update_lines (int start_line, int num_lines, const uint8_t * line_data)
 {
-#if (ROWS_PER_PAGE != GDEP015OC1_ROWS)
-	int line = start_line;
-    int end_line;
-
-    if (line < 0 || num_lines != ROWS_PER_PAGE) {
-        return -1;
-    }
-
-    end_line = start_line + num_lines - 1;
-
-    part_display(0x00, 0x18, line, 0x00, end_line, 0x00);    // set ram
-    EPD_W21_WriteDispRam(GDEP015OC1_COLUMNS, ROWS_PER_PAGE, (uint8_t *) line_data);
-    EPD_W21_Update1();
-    driver_delay_xms(10000);
-    part_display(0x00, 0x18, line, 0x00, end_line, 0x00);    // set ram
-    EPD_W21_WriteDispRam(GDEP015OC1_COLUMNS, ROWS_PER_PAGE, (uint8_t *) line_data);
-#else
-    EPD_W21_SetRamPointer(0x00, 0x00, 0x00);  // set ram
-    EPD_W21_WriteDispRam(GDEP015OC1_COLUMNS, GDEP015OC1_ROWS, (uint8_t *) line_data);
-    EPD_W21_Update();
-#endif
-
-    driver_delay_xms(1000);
+    // We probably do not need half of these commands to update the display, but the documentation is so
+    // poor for this display that we'll start with that, and gradually remove commands to speed things up
+    EPD_W21_WriteCMD(0x01);
+    EPD_W21_WriteDATA(0x07);            //ÉèÖÃ¸ßµÍµçÑ¹
+    EPD_W21_WriteDATA(0x00);
+    EPD_W21_WriteDATA(0x08);
+    EPD_W21_WriteDATA(0x00);
+    EPD_W21_WriteCMD(0x06);         //boostÉè¶¨
+    EPD_W21_WriteDATA(0x07);
+    EPD_W21_WriteDATA(0x07);
+    EPD_W21_WriteDATA(0x07);
+    EPD_W21_WriteCMD(0x04);         //ÉÏµç
+    lcd_chkstatus();                //²é¿´Ð¾Æ¬×´Ì¬
+    driver_delay_xms(300000);
+    EPD_W21_WriteCMD(0X00);
+    EPD_W21_WriteDATA(0xcf);                //Ñ¡Ôñ×î´ó·Ö±æÂÊ
+    EPD_W21_WriteCMD(0X50);
+    EPD_W21_WriteDATA(0x27);
+    EPD_W21_WriteCMD(0x30);                 //PLLÉè¶¨
+    EPD_W21_WriteDATA(0x39);
+    EPD_W21_WriteCMD(0x61);                 //ÏñËØÉè¶¨
+    EPD_W21_WriteDATA(0xC8);                //200ÏñËØ
+    EPD_W21_WriteDATA(0x00);                //200ÏñËØ
+    EPD_W21_WriteDATA(0xC8);
+    EPD_W21_WriteCMD(0x82);                 //vcomÉè¶¨
+    EPD_W21_WriteDATA(0x0E);
+    lut_bw();
+    lut_red();
+    pic_display();
+	EPD_W21_WriteCMD(0x10);
+	draw_pic_data_bw(cgram_);
+    // Don't know how to extract red from u8glib yet...
+	//EPD_W21_WriteCMD(0x13);
+	//draw_pic_data_red(okio_logo_only_mouth);
+    EPD_W21_WriteCMD(0x12);
+    lcd_chkstatus();
+    driver_delay_xms(300000);         //wait for fresh display
+    EPD_W21_WriteCMD(0X50);
+    EPD_W21_WriteDATA(0x17);                //BD floating
+    EPD_W21_WriteCMD(0x82);                 //to solve Vcom drop
+    EPD_W21_WriteDATA(0x00);
+    EPD_W21_WriteCMD(0x01);                 //power setting
+    EPD_W21_WriteDATA(0x02);                //gate switch to external
+    EPD_W21_WriteDATA(0x00);
+    EPD_W21_WriteDATA(0x00);
+    EPD_W21_WriteDATA(0x00);
+    driver_delay_xms(1500);                 //delay 1.5S
+    EPD_W21_WriteCMD(0X02);                 //power off
+    driver_delay_xms(40000);
     return 0;
 }
 
@@ -218,14 +250,6 @@ static uint8_t u8g_com_fn (u8g_t *u8g, uint8_t msg, uint8_t arg_val, void *arg_p
     return rc;
 }
 
-static void lcd_chkstatus(void)
-{
-    while(1)
-    {
-        if(isEPD_W21_BUSY==0) break;
-    }
-    driver_delay_xms(2000);
-}
 
 
 static uint8_t u8g_dev_fn (u8g_t * u8g, u8g_dev_t * dev, uint8_t msg, void * arg)
@@ -308,4 +332,4 @@ static uint8_t u8g_dev_fn (u8g_t * u8g, u8g_dev_t * dev, uint8_t msg, void * arg
 #endif
 }
 
-u8g_dev_t xGDEP015OC1u8gDevice = { u8g_dev_fn, &pb, u8g_com_fn };
+u8g_dev_t xGDEW0154T1u8gDevice = { u8g_dev_fn, &pb, u8g_com_fn };
