4eba2a0e00b82fdf73317402cf94e4da65930e34
Javier Cardona <javier.cardona@gmail.com>
2016-11-06 00:35:43 -0700

    Counter updated every 10s, VLPS in between updates

--- source/main.c
+++ source/main.c
@@ -9,6 +9,10 @@
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "display.h"
+#include "fsl_smc.h"
+#include "fsl_lptmr.h"
+#include "fsl_llwu.h"
+
 
 static void wait()
 {
@@ -31,6 +35,40 @@ void (*fn[])() = {
                    NULL
 };
 
+#define LLWU_LPTMR_IDX 0U      /* LLWU_M0IF */
+#define WAKEUP_INTERVAL_S	10
+
+void setWakeupConfig()
+{
+	LPTMR_SetTimerPeriod(LPTMR0, (LPO_CLK_FREQ * WAKEUP_INTERVAL_S) - 1U);
+	LPTMR_StartTimer(LPTMR0);
+	LPTMR_EnableInterrupts(LPTMR0, kLPTMR_TimerInterruptEnable);
+	LLWU_EnableInternalModuleInterruptWakup(LLWU, LLWU_LPTMR_IDX, true);
+	NVIC_EnableIRQ(LLWU_IRQn);
+}
+
+
+/* Interrupt handers, defined in startup/startup_MKL17Z4.S */
+void LLWU_IRQHandler(void)
+{
+    /* If wakeup by LPTMR. */
+    if (LLWU_GetInternalWakeupModuleFlag(LLWU, LLWU_LPTMR_IDX))
+    {
+        LPTMR_DisableInterrupts(LPTMR0, kLPTMR_TimerInterruptEnable);
+        LPTMR_ClearStatusFlags(LPTMR0, kLPTMR_TimerCompareFlag);
+        LPTMR_StopTimer(LPTMR0);
+    }
+}
+
+void LPTMR0_IRQHandler(void)
+{
+    if (kLPTMR_TimerInterruptEnable & LPTMR_GetEnabledInterrupts(LPTMR0))
+    {
+        LPTMR_DisableInterrupts(LPTMR0, kLPTMR_TimerInterruptEnable);
+        LPTMR_ClearStatusFlags(LPTMR0, kLPTMR_TimerCompareFlag);
+        LPTMR_StopTimer(LPTMR0);
+    }
+}
 
 
 /*!
@@ -39,6 +77,8 @@ void (*fn[])() = {
 int main(void)
 {
     int i = 0;
+    lptmr_config_t lptmrConfig;
+
 
     /* Init board hardware. */
     BOARD_InitPins();
@@ -46,15 +86,24 @@ int main(void)
 //    BOARD_BootClockRUN();
     BOARD_InitDebugConsole();
 
+    LPTMR_GetDefaultConfig(&lptmrConfig);
+    lptmrConfig.prescalerClockSource = kLPTMR_PrescalerClock_1; /* Use LPO as clock source. */
+    lptmrConfig.bypassPrescaler = true;
+    LPTMR_Init(LPTMR0, &lptmrConfig);
+
+    NVIC_EnableIRQ(LLWU_IRQn);
+    NVIC_EnableIRQ(LPTMR0_IRQn);
+
     display_init();
-    while (fn[i] != NULL) {
-        fn[i++]();
-    }
+    /*while (fn[i] != NULL) {*/
+        /*fn[i++]();*/
+    /*}*/
 
     i = 0;
     for(;;) { /* Infinite loop to avoid leaving the main function */
-        wait();
-        wait();
+	    setWakeupConfig();
         display_counter(i++);
+		SMC_SetPowerModeVlps(SMC);
     }
+    wait();
 }
