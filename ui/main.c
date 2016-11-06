0810d4df9916c5a4dcf997af2128e16d86b77def
Javier Cardona <javier.cardona@gmail.com>
2016-11-05 23:12:03 -0700

    Testing fonts

--- ui/main.c
+++ ui/main.c
@@ -14,7 +14,10 @@ int u8g_sdl_get_key(void);
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]));
 
-static char initial[] = "Hello OKIO";
+static char initial[] = "Everytime I find";
+static char second[] = "THE MEANING OF LIFE";
+static char numbers1[] = "1234567890";
+static char numbers2[] = "1234567890";
 
 int main(void)
 {
@@ -33,10 +36,30 @@ int main(void)
 
             u8g_SetColorIndex(&u8g, 0);
 
+            u8g_SetFont(&u8g, u8g_unifont_upper);
+            u8g_SetFontPosCenter(&u8g);
+            w = u8g_GetStrWidth(&u8g, initial);
+            u8g_DrawStr(&u8g, WIDTH/2 - w/2, HEIGHT/2 - 20, initial);
+
             u8g_SetFont(&u8g, u8g_font_profont22r);
             u8g_SetFontPosCenter(&u8g);
             w = u8g_GetStrWidth(&u8g, initial);
             u8g_DrawStr(&u8g, WIDTH/2 - w/2, HEIGHT/2, initial);
+
+            u8g_SetFont(&u8g, u8g_font_freedoomr10r);
+            u8g_SetFontPosCenter(&u8g);
+            w = u8g_GetStrWidth(&u8g, second);
+            u8g_DrawStr(&u8g, WIDTH/2 - w/2, HEIGHT/2 + 20, second);
+
+            u8g_SetFont(&u8g, u8g_font_profont22r);
+            u8g_SetFontPosCenter(&u8g);
+            w = u8g_GetStrWidth(&u8g, numbers1);
+            u8g_DrawStr(&u8g, WIDTH/2 - w/2, HEIGHT/2 + 40, numbers1);
+
+            u8g_SetFont(&u8g, u8g_font_freedoomr10r);
+            u8g_SetFontPosCenter(&u8g);
+            w = u8g_GetStrWidth(&u8g, numbers2);
+            u8g_DrawStr(&u8g, WIDTH/2 - w/2, HEIGHT/2 + 60, numbers2);
 
         } while( u8g_NextPage(&u8g) );
 
